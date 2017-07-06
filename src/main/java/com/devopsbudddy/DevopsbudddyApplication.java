c1200f6dc49ff7c703f2db1fe9c07e6ab7a4b9a4
Khetho Borole <kbborole2@gmail.com>
2017-07-06 10:05:40 -0700

    Initial commit for our first Spring boot project

---
+++ src/main/java/com/devopsbudddy/DevopsbudddyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbudddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbudddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbudddyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devopsbudddy/DevopsbudddyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopsbudddy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsbudddyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
